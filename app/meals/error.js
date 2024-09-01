4c9056a10aa87ae58dbd55cccbba25ce85be97ec
GDevWeb <zodiaquestar@gmail.com>
2024-09-01 12:21:45 +0200

    14. Loading & Rendering Meal Details via Dynamic Routes & Route Parameters

--- app/meals/error.js
+++ app/meals/error.js
@@ -1,9 +1,8 @@
 "use client";
 
-import classes from "../../app/page.module.css";
 function Error() {
   return (
-    <main className={classes.error}>
+    <main className="error">
       <h1>An error occurred</h1>
       <p>Failed to fetch meal data. Please try again later</p>
     </main>
